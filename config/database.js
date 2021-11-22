ad0dd0c7fbde795d358c13d16ea9aa8284df7475
Rafael Camelo <rcmelo31@motorola.com>
2021-11-22 16:28:58 -0400

    update database config

--- config/database.js
+++ config/database.js
@@ -7,9 +7,10 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'pg-strapi-faq-rafael'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
+        ssl: { rejectUnauthorized: false },
       },
       options: {
         ssl: false,
