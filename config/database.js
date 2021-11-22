73efd2493253e2c03fb84428e0fe44cca44d0f2d
Rafael Camelo <rcmelo31@motorola.com>
2021-11-22 16:28:58 -0400

    update database config

--- config/database.js
+++ config/database.js
@@ -4,15 +4,24 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
+        // client: 'sqlite',
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'pg-strapi-faq-rafael'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
       },
       options: {
-        ssl: false,
+        ssl: { rejectUnauthorized: false },
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000
+        }
       },
     },
   },
